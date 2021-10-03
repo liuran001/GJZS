b4841b77eba4270e98633ea1fc9a2113f092edab
liuran001 <liuran001@126.com>
2021-10-03 23:29:14 +0800

    修复更新检测

--- app/src/main/assets/usr/kr-script/CheckUpdate.sh
+++ app/src/main/assets/usr/kr-script/CheckUpdate.sh
@@ -14,7 +14,7 @@ if [[ -f $Core ]]; then
 if [[ -f ~/offline || -f ~/offline2 ]]; then
 	V 软件
 else
-    if [[ $Version_code -lt $New_Code ]]; then
+    if [[ "$Version_code" < "$New_Code" ]]; then
         echo "- 当前版本：$Version_Name（$Version_code）"
         echo "- 已发布了最新版本：$New_Version（$New_Code）"
         echo "- 请前往https://gjzs.qqcn.xyz/ 下载最新版本"
