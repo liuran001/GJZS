ae02dada2eca3f14889bcc72e857a7d80d87b4c5
笨蛋ovo <32791471+liuran001@users.noreply.github.com>
2022-01-18 21:34:10 +0800

    修复开机第一屏制作无法生成配置问题

--- app/src/main/assets/usr/kr-script/Install_BootAnimation_Screen1_Block.sh
+++ app/src/main/assets/usr/kr-script/Install_BootAnimation_Screen1_Block.sh
@@ -1,5 +1,1 @@
-#本脚本由　by Han | 情非得已c，编写
-#应用于搞机助手上
-
-
-sh /data/data/gjzs.online/files/kr-script//Block_Device_Name.sh | egrep '/logo|/splash'
+sh $ShellScript/Block_Device_Name.sh | egrep '/logo|/splash'
