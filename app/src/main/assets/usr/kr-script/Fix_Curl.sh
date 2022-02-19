e40411871477faa67e23aeb0748bb48085aea5b7
笨蛋ovo <32791471+liuran001@users.noreply.github.com>
2022-02-20 04:08:12 +0800

    Update Fix_Curl.sh

--- app/src/main/assets/usr/kr-script/Fix_Curl.sh
+++ app/src/main/assets/usr/kr-script/Fix_Curl.sh
@@ -1,5 +1,6 @@
+. $Core
 rm -f $ELF1_Path/curl3
-wget --no-check-certificate -nv -O $ELF1_Path/curl3 "https://file.qqcn.xyz/GJZS/Other/Curl/`getprop ro.product.cpu.abi`"
 rm -f ~/cacert.pem
-wget --no-check-certificate -nv -O ~/cacert.pem "https://file.qqcn.xyz/GJZS/Other/Curl/cacert.pem"
+downloader "$ELF1_Path/curl3" "https://mscdnfile.qqcn.xyz/GJZS/Other/Curl/`getprop ro.product.cpu.abi`"
+downloader ~/cacert.pem "https://mscdnfile.qqcn.xyz/GJZS/Other/Curl/cacert.pem"
 chmod +x $ELF1_Path/curl3
