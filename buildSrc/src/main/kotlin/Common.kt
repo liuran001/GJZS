82c31daaa433de6390f9b2248d843a135173bfe2
James Clef <qwq233@qwq2333.top>
2022-06-09 04:28:07 +0800

    feat: refactor getversionCode to get commit count (#8)
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- buildSrc/src/main/kotlin/Common.kt
+++ buildSrc/src/main/kotlin/Common.kt
@@ -1,4 +1,6 @@
 // 参考https://git.io/JgX5l 和 https://www.jianshu.com/p/582939dfd73e
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.internal.storage.file.FileRepository
 import org.gradle.api.Project
 import java.io.File
 
@@ -30,9 +32,39 @@ object Common {
     }
 
     @JvmStatic
-    fun getversionCode(): Int {
-        //获取当前时间戳加上400000000后输出结果作为versionCode，这样确保了版本号不会比原来的小
-        return (System.currentTimeMillis() / 1000L + 400000000).toInt()
+    fun getversionCode(project: Project): Int {
+        // .git/HEAD描述当前目录所指向的分支信息，内容示例："ref: refs/heads/master\n"
+        val headFile = File(project.rootProject.projectDir, ".git" + File.separator + "HEAD")
+        if (headFile.exists()) {
+            var commitHash: String
+            val strings = headFile.readText(Charsets.UTF_8).split(" ")
+            if (strings.size > 1) {
+                val refFilePath = ".git" + File.separator + strings[1];
+                // 根据HEAD读取当前指向的hash值，路径示例为：".git/refs/heads/master"
+                val refFile = File(project.rootProject.projectDir, refFilePath.replace("\n", "").replace("\r", ""));
+                // 索引文件内容为hash值+"\n"，
+                commitHash = refFile.readText(Charsets.UTF_8)
+            } else {
+                commitHash = strings[0]
+            }
+            commitHash = commitHash.trim()
+            val repo = FileRepository(project.rootProject.file(".git"))
+            val refId = repo.resolve(commitHash)
+            val iterator = Git(repo).log().add(refId).call().iterator()
+            var commitCount = 0
+            while (iterator.hasNext()) {
+                commitCount++
+                iterator.next()
+            }
+            repo.close()
+            println("commit Count: $commitCount")
+            val targetVersionCode = commitCount + 1700000000 // 兼容性方案 整数好看点
+            println("target versionCode: $targetVersionCode")
+            return targetVersionCode
+        } else {
+            println("WARN: .git/HEAD does NOT exist")
+            return 1700000000
+        }
     }
 
 
