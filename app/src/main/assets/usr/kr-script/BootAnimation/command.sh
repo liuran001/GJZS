d02d23f2fe49edf7f5d2818f63379c1385b7a960
笨蛋ovo <github@bdovo.cc>
2022-07-28 08:21:53 +0800

    fix: BootAnimation maker page failure

--- app/src/main/assets/usr/kr-script/BootAnimation/command.sh
+++ app/src/main/assets/usr/kr-script/BootAnimation/command.sh
@@ -3,8 +3,7 @@ FILE="/data/adb/bootanimation_make"
 FFMPEG="$PREFIX/ffmpeg"
 FFMPEGMD5="6151938ef180f016d16332d8955c2816"
 mkdir -p $FILE
-if [[ ! -f $FFMPEG ]]
-then
+if [[ ! -f $FFMPEG ]]; then
 	echo "--联网下载ffmpeg，稍等"
 	curl -o ${FFMPEG} -L "https://s1.gjzsr.com:2083/GJZS/Other/ffmpeg_all"
 fi
